80515fbeef3f61345dd8a6eec46bfd6e3e62b6b7
Anton Goncharuk <antonge92@gmail.com>
2015-11-24 14:25:22 +0200

    Remove githubContributor service. Delete .DS_Store files from the project. Add exclusion to .gitignore.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,7 +5,6 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { HeaderDirective } from '../app/components/header/header.directive';
 import { TranslationService } from '../app/components/translation-service/translation.service';
@@ -30,7 +29,6 @@ angular.module('soopaheroClient',
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .service('translationService', TranslationService)
   .controller('MainController', MainController)
