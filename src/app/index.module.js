ec5a939021b9d137a7b4c82edea07ffb96b95900
Anton Goncharuk <antonge92@gmail.com>
2015-12-07 12:07:03 +0200

    Delete webDevTec service and Malarkey directive. Clean up index.module.js. Clean up index.sass. Clean up main.controller.js

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -7,14 +7,12 @@ import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { LoginController } from '../app/components/login/login.controller';
 import { ForgotPasswordController } from '../app/components/login/forgot-password/forgot-password.controller';
-//import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { HeaderDirective } from '../app/components/header/header.directive';
 import { DialogBgDirective } from '../app/components/dialog/dialog-bg.directive';
 import { DialogCloseDirective } from '../app/components/dialog/dialog-close.directive';
 import { DialogService } from '../app/components/dialog/dialog.service';
 import { LoginService } from '../app/components/login/login.service';
 import { FooterDirective } from '../app/components/footer/footer.directive';
-//import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
 angular.module('soopaheroClient',
   ['ngAnimate',
@@ -29,17 +27,14 @@ angular.module('soopaheroClient',
     'toastr',
     'pascalprecht.translate',
     'tmh.dynamicLocale'])
-  //.constant('malarkey', malarkey)
   //.constant('moment', moment)
   .config(config)
   .config(routerConfig)
   .constant('SH_CONSTANTS', constants)
   .run(runBlock)
-  //.service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
   .controller('LoginController', LoginController)
   .controller('ForgotPasswordController', ForgotPasswordController)
-  //.directive('acmeMalarkey', MalarkeyDirective)
   .service('dialogService', DialogService)
   .service('loginService', LoginService)
   .directive('shHeader', HeaderDirective)
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,37 +1,11 @@
 export class MainController {
-  constructor ($timeout, toastr, loginService) {
+  constructor (loginService) {
     'ngInject';
 
     this.loginService = loginService;
-    this.awesomeThings = [];
-    this.classAnimation = '';
-    this.creationDate = 1448130894261;
-    this.toastr = toastr;
-
-    //this.activate($timeout, webDevTec);
   }
 
   openLoginForm(e) {
     this.loginService.showLoginDialog(e);
-  }
-
-  activate($timeout, webDevTec) {
-    this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
-
-  getWebDevTec(webDevTec) {
-    this.awesomeThings = webDevTec.getTec();
-
-    angular.forEach(this.awesomeThings, (awesomeThing) => {
-      awesomeThing.rank = Math.random();
-    });
-  }
-
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
   }
 }
