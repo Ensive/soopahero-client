c22a717b46667b2040088b1410965da0a831d986
Anton Goncharuk <antonge92@gmail.com>
2015-12-14 14:32:56 +0200

    Add ng-click in the footer to open signUp dialog. Add main controller to <body> tag. Move footer and header directives to index.html. Add two new states: exploits and dashboard (the same). Add posts folder (controller and html). Delete main.html

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,12 +1,29 @@
 export function routerConfig($stateProvider, $urlRouterProvider) {
   'ngInject';
+
   $stateProvider
+
     .state('home', {
       url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
+      templateUrl: 'app/posts/posts.html',
+      controller: 'PostsController',
+      controllerAs: 'vm'
     })
+
+    .state('dashboard', {
+      url: '/dashboard',
+      templateUrl: 'app/posts/posts.html',
+      controller: 'PostsController',
+      controllerAs: 'vm'
+    })
+
+    .state('exploits', {
+      url: '/exploits',
+      templateUrl: 'app/posts/posts.html',
+      controller: 'PostsController',
+      controllerAs: 'vm'
+    })
+
     .state('users', {
       url: '/heroes',
       templateUrl: 'app/users/users.html',
