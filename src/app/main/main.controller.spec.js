c27c9f5c842ed640672eae938ef635b568ba1c1d
Anton Goncharuk <antonge92@gmail.com>
2015-12-27 02:57:58 +0200

    Do some tests in main.controller.spec.js

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -3,13 +3,31 @@ describe('Main Controller', () => {
 
   beforeEach(angular.mock.module('soopaheroClient'));
 
-  beforeEach(inject(($controller) => {
+  beforeEach(inject(($controller, loginService, signUpService) => {
+    spyOn(loginService, 'showLoginDialog');
+    spyOn(signUpService, 'showSignUpDialog');
     vm = $controller('MainController');
   }));
 
   it('should have access to loginService and signUpService', () => {
     expect(vm.loginService).toEqual(jasmine.any(Object));
     expect(vm.signUpService).toEqual(jasmine.any(Object));
+  });
+
+  it('should open login dialog', () => {
+    let showLoginDialog = vm.loginService.showLoginDialog;
+    vm.openLoginForm();
+
+    expect(showLoginDialog).toHaveBeenCalled();
+    expect(showLoginDialog.calls.count()).toBe(1);
+  });
+
+  it('should open sign-up dialog', () => {
+    let showSignUpDialog = vm.signUpService.showSignUpDialog;
+    vm.openSignUpForm();
+
+    expect(showSignUpDialog).toHaveBeenCalled();
+    expect(showSignUpDialog.calls.count()).toBe(1);
   });
 
 });
