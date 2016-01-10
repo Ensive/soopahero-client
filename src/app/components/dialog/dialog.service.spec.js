0897fc416c1cdd370672e9248db4c399a122458a
Anton Goncharuk <antonge92@gmail.com>
2016-01-10 22:52:45 +0200

    Write simple tests for DialogService (add spec).

---
+++ src/app/components/dialog/dialog.service.spec.js
@@ -0,0 +1,36 @@
+import { sharedSetup } from '../../../../spec/helper.spec';
+
+describe('dialogService', function () {
+  var mdDialog;
+
+  sharedSetup();
+
+  beforeEach(inject(($mdDialog, dialogService) => {
+    mdDialog = $mdDialog;
+
+    spyOn(dialogService, 'showAdvancedDialog');
+    spyOn(dialogService, 'hideDialog');
+  }));
+
+  it('should be registered', inject(dialogService => {
+    expect(dialogService).not.toEqual(null);
+    expect(dialogService.mdDialog).toEqual(mdDialog);
+  }));
+
+  describe('show advanced dialog function', () => {
+    it('should be defined and callable', inject(function (dialogService) {
+      dialogService.showAdvancedDialog({}, {});
+      expect(dialogService.showAdvancedDialog).toHaveBeenCalled();
+      expect(dialogService.showAdvancedDialog.calls.count()).toBe(1);
+    }));
+  });
+
+  describe('hide dialog function', function () {
+    it('should be defined and callable', inject(function (dialogService) {
+      dialogService.hideDialog();
+      expect(dialogService.hideDialog).toHaveBeenCalled();
+    }));
+  });
+
+
+});
